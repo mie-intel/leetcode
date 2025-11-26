273fa962aebe6a1b1c089392352ccdd5bd7e7ff1
mie-intel <103818115+mie-intel@users.noreply.github.com>
2025-11-26 16:40:51 +0700

    Time: 183 ms (89.1%), Space: 171.1 MB (13.98%) - LeetHub

---
+++ 2435-paths-in-matrix-whose-sum-is-divisible-by-k/2435-paths-in-matrix-whose-sum-is-divisible-by-k.cpp
@@ -0,0 +1,38 @@
+typedef long long ll;
+
+const ll mod = 1e9 + 7;
+class Solution {
+public:
+    int numberOfPaths(vector<vector<int>>& grid, int k) {
+        vector <vector <vector <ll>>> g;
+        
+        int n = grid.size();
+        int m = grid[0].size();
+        for(int i = 0; i < n; ++i){
+            g.push_back(vector <vector <ll>> (m, vector <ll> (k, 0)));
+        }
+        g[0][0][grid[0][0] % k] = 1;
+        ll sum = grid[0][0] % k;
+        for(int i = 1; i < n; ++i){
+            sum += grid[i][0];
+            sum %= k;
+            g[i][0][sum] = 1;
+        }
+        sum = grid[0][0] % k;
+        for(int i = 1; i < m; ++i){
+            sum += grid[0][i];
+            sum %= k;
+            g[0][i][sum] = 1;
+        }
+        for(int i = 1; i < n; ++i){
+            for(int j = 1; j < m; ++j){
+                for(int ki = 0; ki < k; ++ki){
+                    int nx = (ki + grid[i][j]) % k;
+                    g[i][j][nx] += (g[i-1][j][ki] + g[i][j-1][ki]);
+                    g[i][j][nx] %= mod;
+                }
+            }
+        }
+        return g[n-1][m-1][0];
+    }
+};
