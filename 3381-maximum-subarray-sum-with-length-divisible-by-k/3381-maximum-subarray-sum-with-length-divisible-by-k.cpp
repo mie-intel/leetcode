9b599f7dd76cda3b2672c0aa27bd62c9fe805b73
mie-intel <103818115+mie-intel@users.noreply.github.com>
2025-11-27 07:48:16 +0700

    Time: 2723 ms (5.12%), Space: 202.7 MB (6.48%) - LeetHub

---
+++ 3381-maximum-subarray-sum-with-length-divisible-by-k/3381-maximum-subarray-sum-with-length-divisible-by-k.cpp
@@ -0,0 +1,29 @@
+typedef long long ll;
+
+class Solution {
+public:
+    long long maxSubarraySum(vector<int>& nums, int k) {
+        vector <ll> pref = {0};
+        for(auto p : nums){
+            pref.push_back(pref.back() + p);
+        }
+        int n = nums.size();
+        ll mx = -1e15;
+        vector <ll> dp = vector <ll> (n+1, -1e15);
+        for(int i = k; i <= n; ++i){
+            if(dp[i-k] == -1e15){
+                dp[i] = pref[i] - pref[i-k];
+            }
+            else{
+                dp[i] = max(dp[i], dp[i-k] + pref[i] - pref[i-k]);
+                dp[i] = max(dp[i], pref[i] - pref[i-k]);
+            }
+            mx = max(mx, dp[i]);
+        }
+        for(auto p : dp){
+            cout << p << " ";
+        }
+        cout << '\n';
+        return mx;
+    }
+};
