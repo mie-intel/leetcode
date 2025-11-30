8a9fe743852cbe4348cb46b7645d2e8b33b97ded
mie-intel <103818115+mie-intel@users.noreply.github.com>
2025-11-30 09:18:48 +0700

    Time: 2095 ms (5.29%), Space: 91.1 MB (17.73%) - LeetHub

---
+++ 1590-make-sum-divisible-by-p/1590-make-sum-divisible-by-p.cpp
@@ -0,0 +1,24 @@
+typedef long long ll;
+class Solution {
+public:
+    int minSubarray(vector<int>& nums, int p) {
+        map <ll, ll> ls;
+        ls[0] = -1;
+        ll sm = 0;
+        ll ans = INT_MAX;
+        ll total = 0;
+        for(auto p : nums) total += p;
+        total %= p;
+        for(int i = 0; i < nums.size(); ++i){
+            sm += nums[i];
+            sm %= p;
+            ls[sm] = i;
+            ll tar = (sm - total + p + p) % p;
+            if(ls.count(tar)){
+                ans = min(ans, i - ls[tar]);
+            }
+            cout << sm << " " << tar << '\n';
+        }
+        return ans >= nums.size() ? -1 : ans;
+    }
+};
