779b84fefa9211c21f7a8a2365afe28b7e442b63
mie-intel <103818115+mie-intel@users.noreply.github.com>
2025-12-08 15:08:00 +0700

    Time: 0 ms (100%), Space: 7.8 MB (55.75%) - LeetHub

--- 1925-count-square-sum-triples/1925-count-square-sum-triples.cpp
+++ 1925-count-square-sum-triples/1925-count-square-sum-triples.cpp
@@ -3,10 +3,10 @@ public:
     int countTriples(int n) {
         int cnt = 0;
         for(int i = 1; i <= n; ++i){
-            for(int j = 1; j <= n; ++j){
+            for(int j = i; j <= n; ++j){
                 int sq = sqrt(i * i + j * j);
                 if(sq > n) break;
-                cnt += ((sq * sq == i * i + j * j) && 1 <= sq && sq <= n);
+                cnt += ((sq * sq == i * i + j * j) && 1 <= sq && sq <= n) * (i != j ? 2 : 1);
             }
         }
         return cnt;
