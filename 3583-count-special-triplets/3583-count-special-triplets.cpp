10b4bef72eb8429a648ea91443e6ae7202dd70d6
mie-intel <103818115+mie-intel@users.noreply.github.com>
2025-12-09 09:50:46 +0700

    Time: 1469 ms (10.21%), Space: 431.6 MB (20.44%) - LeetHub

---
+++ 3583-count-special-triplets/3583-count-special-triplets.cpp
@@ -0,0 +1,32 @@
+typedef long long ll;
+
+const int maxn = 1e5 + 1;
+const ll mod = 1e9 + 7;
+
+class Solution {
+public:
+    int specialTriplets(vector<int>& nums) {
+        map <ll, vector <ll>> mp;
+        for(int i = 0; i < nums.size(); ++i){
+            mp[nums[i]].push_back(i);
+        }
+        ll ans = 0;
+        for(int i = 0; i < nums.size(); ++i){
+            int kal = nums[i] * 2;
+            int id = lower_bound(mp[kal].begin(), mp[kal].end(), i) - mp[kal].begin();
+            if(id == mp[kal].size()) continue;
+            if(kal == nums[i]){
+                ll up = mp[kal].size() - id - 1;
+                ll low = id;
+                ans += (up * low % mod);
+            }
+            else{
+                ll up = mp[kal].size() - id;
+                ll low = id;
+                ans += (up * low % mod);
+            }
+            ans %= mod;
+        }
+        return ans;
+    }
+};
