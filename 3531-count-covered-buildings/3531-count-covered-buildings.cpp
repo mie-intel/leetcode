5838b7fe087f7e5c6e1f35b6155ba96f1c29a2b3
mie-intel <103818115+mie-intel@users.noreply.github.com>
2025-12-11 11:46:42 +0700

    Time: 1875 ms (5.52%), Space: 492.9 MB (5.52%) - LeetHub

---
+++ 3531-count-covered-buildings/3531-count-covered-buildings.cpp
@@ -0,0 +1,37 @@
+const int maxn = 1e5 + 5;
+#define fi first
+#define se second
+
+class Solution {
+public:
+    int countCoveredBuildings(int n, vector<vector<int>>& buildings) {
+        int cnt[maxn];
+        memset(cnt, 0, sizeof cnt);
+        map <int, set <pair <int, int>>> x, y;
+        for(int i = 0; i < buildings.size(); ++i){
+            auto p = buildings[i];
+            x[p[0]].emplace(p[1], i);
+            y[p[1]].emplace(p[0], i);
+        }
+        for(auto [q, p]: x){
+            int cz = 0;
+            for(auto c : p){
+                if(cz > 0 && cz < p.size()-1){
+                    cnt[c.se]++;    
+                }
+                cz++;
+            }
+        }
+        int sz = 0;
+        for(auto [q, p]: y){
+            int cz = 0;
+            for(auto c : p){
+                if(cz > 0 && cz < p.size()-1){
+                    sz += cnt[c.se];
+                }
+                cz++;
+            }
+        }
+        return sz;
+    }
+};
