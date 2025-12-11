b842cab26d7f3e8efa22ec46d8407fea9804e4d5
mie-intel <103818115+mie-intel@users.noreply.github.com>
2025-12-11 12:53:47 +0700

    Time: 5 ms (16.44%), Space: 9.6 MB (14.82%) - LeetHub

---
+++ 0843-guess-the-word/0843-guess-the-word.cpp
@@ -0,0 +1,69 @@
+/**
+ * // This is the Master's API interface.
+ * // You should not implement it, or speculate about its implementation
+ * class Master {
+ *   public:
+ *     int guess(string word);
+ * };
+ */
+ #define fi first
+ #define se second
+class Solution {
+public:
+    vector <string> rem;
+    vector <pair <string, int>> fil;
+    int cek(string s, string t){
+        int cnt = 0;
+        for(int i = 0; i < 6; ++i){
+            cnt += (s[i] == t[i]);
+        }
+        return cnt;
+    }
+    void update(){
+        vector <string> tmp;
+        for(auto p : rem){
+            bool c = true;
+            for(auto q : fil){
+                if(cek(q.fi, p) != q.se){
+                    c = false;
+                    break;
+                }
+            }
+            if(c) tmp.push_back(p);
+        }
+        // cout << "TMP: ";
+        // for(auto p : tmp){
+        //     cout << p << " ";
+        // }
+        // cout << '\n';
+        rem = tmp;
+    }
+    void add(string s, int n){
+        fil.emplace_back(s, n);
+    }
+    void findSecretWord(vector<string>& words, Master& master) {
+        rem.clear();
+        for(auto p : words) rem.push_back(p);
+        sort(rem.begin(), rem.end());
+        random_shuffle(rem.begin(), rem.end());
+        set <string> called;
+        int mx = 30;
+        while(rem.size() && mx){
+            // cout << "CALL " << rem[0] << '\n';
+            for(auto p : rem){
+                if(!called.count(p)){
+                    int cnt = master.guess(rem[0]);
+                    add(rem[0], cnt);
+                    update();
+                    called.insert(p);
+                    break;
+                }
+            }
+            mx--;
+            // for(auto p : rem){
+            //     cout << p << " ";
+            // }
+            // cout << '\n';
+        }   
+    }
+};
