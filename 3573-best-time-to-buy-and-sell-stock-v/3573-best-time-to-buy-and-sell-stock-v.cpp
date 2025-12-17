e99233c3adfc89cdeed6b243516ca1e15b5335d3
mie-intel <103818115+mie-intel@users.noreply.github.com>
2025-12-17 09:26:16 +0700

    Time: 2942 ms (5.07%), Space: 36.3 MB (78.05%) - LeetHub

--- 3573-best-time-to-buy-and-sell-stock-v/3573-best-time-to-buy-and-sell-stock-v.cpp
+++ 3573-best-time-to-buy-and-sell-stock-v/3573-best-time-to-buy-and-sell-stock-v.cpp
@@ -8,9 +8,11 @@ class Solution {
 public:
     long long maximumProfit(vector<int>& p, int k) {
         int n = p.size();
+        ll mx = 0;
+        int mn = 0;
         memset(dp, 0, sizeof dp);
         for(int i = 1; i < n; ++i){
-            int mn = min(k, i/2+1);
+            mn = min(k, i/2+1);
             for(int c = 1; c <= mn; ++c){
                 for(int j = i - 1; j >= ((c-1) * 2); --j){
                     ll add = (j ? dp[j-1][c-1] : 0);
@@ -18,11 +20,8 @@ public:
                 }
                 dp[i][c] = max(dp[i][c], dp[i-1][c]);
             }
-            // if(mn < k) dp[i][mn+1] = max(dp[i][mn+1], dp[i-1][mn+1]);
-            // for(int j = 0; j <= k; ++j) dp[i][j] = max(dp[i][j], dp[i-1][j]);
         }
 
-        ll mx = 0;
         for(int j = 0; j <= k; ++j){
             mx = max(dp[n-1][j], mx);
         }
