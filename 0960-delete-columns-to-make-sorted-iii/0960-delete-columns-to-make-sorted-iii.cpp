267214450fc68b583b1390e99dcdd0266b500d9e
mie-intel <103818115+mie-intel@users.noreply.github.com>
2025-12-22 09:33:48 +0700

    Time: 4 ms (57.44%), Space: 13.3 MB (80%) - LeetHub

---
+++ 0960-delete-columns-to-make-sorted-iii/0960-delete-columns-to-make-sorted-iii.cpp
@@ -0,0 +1,25 @@
+class Solution {
+public:
+    int minDeletionSize(vector<string>& strs) {
+        int n = strs.size();
+        int m = strs[0].length();
+        vector <bool> A;
+        int dp[101];
+        auto cek = [&](int i, int j) -> bool{
+            for(int a = 0; a < n; ++a){
+                if(strs[a][i] > strs[a][j]) return false;
+            }
+            return true;
+        };
+        int mx = 1;
+        dp[0] = 1;
+        for(int i = 1; i < m; ++i){
+            dp[i] = 1;
+            for(int j = 0; j < i; ++j){
+                if(cek(j, i)) dp[i] = max(dp[i], dp[j]+1);
+            }
+            mx = max(mx, dp[i]);
+        }
+        return m - mx;
+    }
+};
