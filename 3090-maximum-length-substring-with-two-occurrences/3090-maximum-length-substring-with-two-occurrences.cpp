30219d06a43741135bbd5fd4d39286857af27060
mie-intel <paryapradhanika.dev@gmail.com>
2026-08-14 08:07:14 +0700

    Time: 27 ms (12.38%), Space: 19.6 MB (5.63%) - LeetHub

---
+++ 3090-maximum-length-substring-with-two-occurrences/3090-maximum-length-substring-with-two-occurrences.cpp
@@ -0,0 +1,15 @@
+class Solution {
+public:
+    int maximumLengthSubstring(string s) {
+        int mx = 0;
+        for(int i = 0; i < s.length(); ++i){
+            map <char, int> mp;
+            for(int j = i; j < s.length(); ++j){
+                mp[s[j]]++;
+                if(mp[s[j]] >= 3) break;
+                else mx = max(mx, j - i + 1);
+            }
+        }
+        return mx;
+    }
+};
