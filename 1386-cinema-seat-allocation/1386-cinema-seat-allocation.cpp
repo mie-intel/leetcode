25081c442c1d3fc9fd944bb250310879a886cc5c
mie-intel <paryapradhanika.dev@gmail.com>
2026-08-19 09:39:25 +0700

    Time: 498 ms (5.07%), Space: 79.9 MB (26.86%) - LeetHub

---
+++ 1386-cinema-seat-allocation/1386-cinema-seat-allocation.cpp
@@ -0,0 +1,24 @@
+class Solution {
+public:
+    int maxNumberOfFamilies(int n, vector<vector<int>>& r) {
+        set <int> st;
+        unordered_map <int, bool> ll;
+        unordered_map <int, bool> mi;
+        unordered_map <int, bool> rr;
+        for(int i = 0; i < r.size(); ++i){
+            st.insert(r[i][0]);
+            if(1 < r[i][1] && r[i][1] < 6) ll[r[i][0]] = 1;
+            if(3 < r[i][1] && r[i][1] < 8) mi[r[i][0]] = 1;
+            if(5 < r[i][1] && r[i][1] < 10) rr[r[i][0]] = 1;
+        }
+        int ans = n * 2;
+        for(auto p : st){
+            if(ll[p] && rr[p] && !mi[p]) ans -= 1;
+            else if(ll[p] && rr[p] && mi[p]) ans -= 2;
+            else if(mi[p]) ans -= int(!ll[p]) + int(!rr[p]);
+            else if(int(ll[p]) + int(rr[p]) + int(mi[p]) == 1) ans--;
+            cout << p << " " << ans << '\n';
+        }
+        return ans;
+    }
+};
